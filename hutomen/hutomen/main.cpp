e59fa03a05a3015d90bc63b68d75b080a1c6c47c
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-01-13 16:24:25 +0900

    Update.

--- hutomen/hutomen/main.cpp
+++ hutomen/hutomen/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 	try {
 		const QDateTime dt = QDateTime::currentDateTime();
 		const QString date = dt.toString("yyyy/MM/dd hh:mm:ss");
-		const QString name = Jstr("Œ»İ“ú : ");
+		const QString name = Jstr("Current Date and Time : ");
 		
 		QTextStream(stdout) << name << date << endl;
 		return 0;
